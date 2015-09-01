bcc578aad36ca140074183ef08df75504fe1c37c
Balint Erdi <balint.erdi@gmail.com>
2015-09-01 21:06:54 +0200

    Use objects instead of strings

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,8 +1,18 @@
 import Ember from 'ember';
 
-const { Controller } = Ember;
-
-export default Controller.extend({
+export default Ember.Controller.extend({
 	favoriteBand: null,
-  bands: ['Pearl Jam', 'Tool', 'Long Distance Calling', 'Led Zeppelin'],
+  bands: [
+    Ember.Object.create({ id: "1", name: 'Pearl Jam', formedIn: 1990 }),
+    Ember.Object.create({ id: "2", name: 'Tool', formedIn: 1991 }),
+    Ember.Object.create({ id: "3", name: 'Long Distance Calling', formedIn: 2003 }),
+    Ember.Object.create({ id: "4", name: 'Led Zeppelin', formedIn: 1970 })
+  ],
+
+  actions: {
+    setFavorite(bandId) {
+      const newFav = this.get('bands').findBy('id', bandId);
+      this.set('favoriteBand', newFav);
+    }
+  }
 });
