1da859dbdac66e0fa448b1fc782c79b8f91f016f
Balint Erdi <balint.erdi@gmail.com>
2016-04-14 15:00:03 +0200

    Set the selected band as a query parameter

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,18 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-	favoriteBand: null,
+  queryParams: ['bandId'],
+
+  bandId: null,
+
+  favoriteBand: Ember.computed('bandId', function() {
+    return this.get('bands').findBy('id', this.get('bandId'));
+  }),
+
   bands: [
     Ember.Object.create({ id: "1", name: 'Pearl Jam', formedIn: 1990 }),
     Ember.Object.create({ id: "2", name: 'Tool', formedIn: 1991 }),
     Ember.Object.create({ id: "3", name: 'Long Distance Calling', formedIn: 2003 }),
     Ember.Object.create({ id: "4", name: 'Led Zeppelin', formedIn: 1970 })
-  ],
-
-  actions: {
-    setFavorite(bandId) {
-      const newFav = this.get('bands').findBy('id', bandId);
-      this.set('favoriteBand', newFav);
-    }
-  }
+  ]
 });
