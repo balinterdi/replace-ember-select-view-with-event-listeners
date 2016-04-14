a373de278df1d64aba19fe61a2f755268474ba2f
Balint Erdi <balint.erdi@gmail.com>
2016-04-14 14:33:38 +0200

    Upgrade to 2.4.3 and us a custom eq helper

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,18 +1,8 @@
 import Ember from 'ember';
 
-export default Ember.Controller.extend({
-	favoriteBand: null,
-  bands: [
-    Ember.Object.create({ id: "1", name: 'Pearl Jam', formedIn: 1990 }),
-    Ember.Object.create({ id: "2", name: 'Tool', formedIn: 1991 }),
-    Ember.Object.create({ id: "3", name: 'Long Distance Calling', formedIn: 2003 }),
-    Ember.Object.create({ id: "4", name: 'Led Zeppelin', formedIn: 1970 })
-  ],
+const { Controller } = Ember;
 
-  actions: {
-    setFavoriteBand(bandId) {
-      const newFav = this.get('bands').findBy('id', bandId);
-      this.set('favoriteBand', newFav);
-    }
-  }
+export default Controller.extend({
+	favoriteBand: null,
+  bands: ['Pearl Jam', 'Tool', 'Long Distance Calling', 'Led Zeppelin'],
 });
